473bae060e68ad9f398ce42d4dcdeee00d18020c
l-n-s <l-n-s@sigaint.org>
2015-08-01 09:25:00 +0300

    Added sources, edited README

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # i2pd-webui
 Fancy i2pd web user interface
+
+# Usage:
+
+Git clone repo to your computer.
+
+Run i2pd with i2pcontrol port listening at 7650, for example:
+
+    ./i2p --i2pcontrolport=7650
+
+Then run web ui:
+
+    python run.py
+
+Visit http://127.0.0.1:8082/ and enjoy fancy i2pd webui! ;)
---
+++ run.py
@@ -0,0 +1,61 @@
+import BaseHTTPServer
+import urllib2
+from os import sep
+
+listen_port = 8082
+i2pcontrol_url = "http://127.0.0.1:7650/"
+
+def resp_html(s):
+    """Response to GET requests with actual documents"""
+    legal_files = ["/js/lib/underscore-min.js", "/js/app.js", "/favicon.ico", "/css/main.css"]
+
+    if s.path == "/":
+        s.send_response(200)
+        s.send_header("Content-Type", "text/html")
+        s.end_headers()
+        with open("index.html") as f:
+            s.wfile.write(f.read())
+    elif s.path in legal_files:
+        file_path = s.path[1:].replace("/", sep)
+        with open(file_path) as f:
+            s.wfile.write(f.read())
+    else:
+        s.send_error(404, "Not Found")
+
+def proxy_request(data, s):
+    """Send data to i2pcontrol port and return response"""
+    
+    req = urllib2.Request(i2pcontrol_url, data)
+    print(data)
+    try:
+        response = urllib2.urlopen(req)
+        s.send_response(200)
+        s.send_header("Content-Type", "application/json")
+        s.end_headers()
+        resp = response.read()
+        print(resp)
+        s.wfile.write(resp)
+    except urllib2.URLError:
+        s.send_error(500, "Cannot connect to I2PControl port")
+    except:
+        s.send_error(500, "Error")
+
+
+class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    def do_GET(s):
+        resp_html(s)
+
+    def do_POST(s):
+        content_length = int(s.headers['Content-Length'])
+        post_data = s.rfile.read(content_length).decode('utf-8')
+        proxy_request(post_data, s)
+
+if __name__ == "__main__":
+    httpd = BaseHTTPServer.HTTPServer(("", listen_port), MyHandler)
+    try:
+        print("WebUI is listening at http://127.0.0.1:" + str(listen_port))
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    httpd.server_close()
