aea108590f0a1ea3830d681287e1c1e1e13fa4b8
l-n-s <l-n-s@sigaint.org>
2015-08-07 14:00:29 +0300

    Added days to uptime string

--- js/app.js
+++ js/app.js
@@ -45,11 +45,12 @@ var password = "itoopie";
 function msToString(mseconds) {
     // ms to string for uptime
     var seconds = mseconds / 1000;
+    var numdays = Math.floor(seconds / 86400);
     var numhours = Math.floor((seconds % 86400) / 3600);
     var numminutes = Math.floor(((seconds % 86400) % 3600) / 60);
     var numseconds = ((seconds % 86400) % 3600) % 60;
 
-    return numhours + "h " + numminutes + "m " + numseconds + "s";
+    return numdays + "d " + numhours + "h " + numminutes + "m " + numseconds + "s";
 }
 
 function doAjax(responseHandler, data) {
